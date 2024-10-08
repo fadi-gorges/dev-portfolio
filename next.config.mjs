d3e1b4247b1130f79640dfc64a0d75fa01b28185
fadi-gorges <Fadi.Gorges@student.uts.edu.au>
2024-10-09 01:51:33 +1100

    Remove sharp dependency and configure Next.js for unoptimized images

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withContentlayer } from "next-contentlayer";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default withContentlayer(nextConfig);
