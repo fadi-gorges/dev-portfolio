261c765d09ab57afd87dac317e4e8cead1d32386
fadi-gorges <Fadi.Gorges@student.uts.edu.au>
2024-10-08 17:56:36 +1100

    Add Dockerfile and .dockerignore for containerization; configure Next.js for standalone output

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 import { withContentlayer } from "next-contentlayer";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "standalone",
+};
 
 export default withContentlayer(nextConfig);
